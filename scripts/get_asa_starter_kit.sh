31ff4111dce04856714b3b37dcda18696192bc42
dsilvest <davesilvestro@yahoo.com>
2021-08-19 14:12:06 -0500

    Update get_asa_starter_kit.sh

--- scripts/get_asa_starter_kit.sh
+++ scripts/get_asa_starter_kit.sh
@@ -2,4 +2,3 @@
 curl https://codeload.github.com/okta-server-asa/StarterKit/zip/refs/heads/master --output asa_starter_kit.zip
 unzip asa_starter_kit.zip
 cd StarterKit-master/AWS/ubuntu-alone
-cp -r * ../../../../5_app_server/
